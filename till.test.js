5470874544379e1c28890a2a084b454fb7d85d99
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-23 14:48:37 +0100

    unit test for getItems()

--- till.test.js
+++ till.test.js
@@ -6,10 +6,17 @@ describe('Till', () => {
     jest.setSystemTime(new Date('2022-08-07'));
   });
 
-  it('adds an item to the list', () => {
+  describe('empty till', () => {
     const till = new Till();
-    till.addItem('Flat White');
 
-    expect(till.items).toEqual(['Flat White']);
+    it('returns items in till', () => {
+      expect(till.getItems()).toEqual([]);
+    });
+
+    it('adds an item to the list', () => {
+      till.addItem('Flat White');
+
+      expect(till.items).toEqual(['Flat White']);
+    });
   });
 });
