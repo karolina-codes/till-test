1dc68ff02fe11699cdbd0aba5d8cbab8d054a387
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-23 16:37:22 +0100

    test for totalPrice()

--- till.test.js
+++ till.test.js
@@ -21,15 +21,32 @@ describe('Till', () => {
   });
 
   describe('adds multiple items to the till in one go', () => {
-    const till = new Till();
-
     it('adds 3 items to the list', () => {
+      const till = new Till();
       till.addItems('Affogato', 'Tiramisu', 'Cappucino');
+
       expect(till.getItems()).toEqual({
         Affogato: [1, 14.8],
         Cappucino: [1, 3.85],
         Tiramisu: [1, 11.4],
       });
     });
+
+    it('adds 2 of the same item to the list', () => {
+      const till = new Till();
+
+      till.addItems('Affogato', 'Affogato');
+
+      expect(till.getItems()).toEqual({
+        Affogato: [2, 14.8],
+      });
+    });
+  });
+
+  it('returns the total price for all items', () => {
+    const till = new Till();
+    till.addItems('Affogato', 'Cappucino', 'Affogato');
+
+    expect(till.totalPrice()).toEqual(33.45);
   });
 });
